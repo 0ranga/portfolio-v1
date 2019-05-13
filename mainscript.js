8be3f0bfed2ea61fe286d0560e1edd192a27f709
mercvre <44339274+mercvre@users.noreply.github.com>
2019-05-13 18:42:23 +0200

    Add files via upload

--- mainscript.js
+++ mainscript.js
@@ -1,55 +1,73 @@
+//random positioning of projectelement
+// function position() {
+//   $(".projectelement").each(function() {
+//     $(this).children().first().removeClass("left");
+//     $(this).children().first().removeClass("center");
+//     $(this).children().first().removeClass("right");
+//     var i = Math.floor(Math.random() * 3);
+//     if (i == 0) {
+//       $(this).children().first().addClass("left");
+//     }
+//     if (i == 1) {
+//       $(this).children().first().addClass("center");
+//     }
+//     if (i == 2) {
+//       $(this).children().first().addClass("right");
+//     }
+//   });
+// }
+
+//shuffle order of div inside #shuffle
+// function shuffle() {
+//   var parent = $("#shuffle");
+//   var divs = parent.children();
+//   while (divs.length) {
+//     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+//   }
+// }
+
+
+//scroll back to top when images inside #shuffle are loaded
+function totop() {
+  $('#shuffle').imagesLoaded(function() {
+    window.scrollTo(0, 0);
+  });
+}
+
+function showCategory(category) {
+  $(".project").each(function() {
+    if (!$(this).hasClass(category)) {
+      $(this).css({
+        'display': 'none'
+      });
+    }
+    if ($(this).hasClass(category)) {
+      $(this).css({
+        'display': 'block'
+      });
+    }
+  });
+  $("#myDropdown").toggleClass("hidden flexbox");
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownFunction() {
+  $("#myDropdown").toggleClass("hidden flexbox");
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     console.log($(".dropdown-content").is(":visible"));
+//     if ($(".dropdown-content").is(":visible"))
+//     {
+//       $(".dropdown-content").toggle();
+//     }
+//   }
+// }
+
 $(document).ready(function() {
-
-  //random positioning of projectelement
-  // function position() {
-  //   $(".projectelement").each(function() {
-  //     $(this).children().first().removeClass("left");
-  //     $(this).children().first().removeClass("center");
-  //     $(this).children().first().removeClass("right");
-  //     var i = Math.floor(Math.random() * 3);
-  //     if (i == 0) {
-  //       $(this).children().first().addClass("left");
-  //     }
-  //     if (i == 1) {
-  //       $(this).children().first().addClass("center");
-  //     }
-  //     if (i == 2) {
-  //       $(this).children().first().addClass("right");
-  //     }
-  //   });
-  // }
-
-  //shuffle order of div inside #shuffle
-  // function shuffle() {
-  //   var parent = $("#shuffle");
-  //   var divs = parent.children();
-  //   while (divs.length) {
-  //     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-  //   }
-  // }
-
-
-  //scroll back to top when images inside #shuffle are loaded
-  function totop() {
-    $('#shuffle').imagesLoaded(function() {
-      window.scrollTo(0, 0);
-    });
-  }
-
-  function showCategory(category) {
-    $(".project").each(function() {
-      if (!$(this).hasClass(category)) {
-        $(this).css({
-          'display': 'none'
-        });
-      }
-      if ($(this).hasClass(category)) {
-        $(this).css({
-          'display': 'block'
-        });
-      }
-    });
-  }
 
   // if fragment exists
   if (window.location.hash) {
@@ -62,7 +80,20 @@ $(document).ready(function() {
 
 
   //only shows the category that has been clicked in the navigation bar
-  $("nav ul p, nav ul a").click(function() {
+  // $("nav ul p, nav ul a").click(function() {
+  //   //position();
+  //   //shuffle();
+  //   var category = $(this).attr("id");
+  //   if (window.location.pathname != "/index.html") {
+  //     window.location = "/index.html#category=" + category;
+  //   } else {
+  //     totop();
+  //     showCategory(category);
+  //   }
+  //
+  // });
+
+  $("nav .outside, nav .inside").click(function() {
     //position();
     //shuffle();
     var category = $(this).attr("id");
@@ -74,6 +105,8 @@ $(document).ready(function() {
     }
 
   });
+
+
 
   //only shows the project that has been clicked
   // $(".projectelement").click(function() {
